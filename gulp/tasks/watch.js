2df4e7bd4280b6521a020c17e2449ff4d4681d92
Vladimir Abramyants <vabramanc@gmail.com>
2019-05-27 21:38:29 +0300

    Added new watch

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,5 +4,6 @@ module.exports = () => {
         $.gulp.watch('src/static/sass/**/*.scss', $.gulp.series('sass'));
         $.gulp.watch('src/static/js/main.js', $.gulp.series('scripts'));
         $.gulp.watch('src/static/img/*', $.gulp.series('img:dev'));
+        $.gulp.watch('src/static/img/svg/*', $.gulp.series('svg'));
     });
 }

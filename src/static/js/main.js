4e1a10e45d88c8079f866dca95222db995d29887
Vladimir Abramyants <vabramanc@gmail.com>
2019-05-28 10:33:54 +0300

    added function for slider

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,9 +1,49 @@
-let btn = document.getElementById('btn');
-let prev = document.getElementById('prev');
+// function
+let sliderIndex = 1;
+showSlides(sliderIndex);
 
-let handler = () => {
-    alert('Hello');
+function incrementSlide () {
+    showSlides(sliderIndex += 1);
 }
 
-prev.addEventListener('click', handler);
-btn.addEventListener('click', handler);
+function decrementSlide() {
+    showSlides(sliderIndex -= 1);
+}
+
+function currentSlide(n) {
+    showSlides(sliderIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName('slider__item');
+    let dots = document.getElementsByClassName('slider-dots__item');
+    console.log(dots);
+
+    if (n > slides.length) {
+        sliderIndex = 1;
+    } else {
+        sliderIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[sliderIndex - 1].style.display = "block";
+    dots[sliderIndex - 1].classList.add(" active");
+}
+
+// let btn = document.getElementById('btn');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let handler = () => { alert('Hello'); }
+
+// btn.addEventListener('click', handler);
+prev.addEventListener('click', incrementSlide());
+next.addEventListener('click', decrementSlide())
--- src/static/js/slider.js
+++ src/static/js/slider.js
@@ -1,3 +1,41 @@
-module.exports = () => {
+let slider = () => {
+    let sliderIndex = 1;
+    showSlides(sliderIndex);
 
+    let incrementSlide = () => {
+        showSlides(sliderIndex += 1);
+    }
+
+    let decrementSlide = () => {
+        showSlides(sliderIndex -= 1);
+    }
+
+    let currentSlide = n => {
+        showSlides(sliderIndex = n);
+    }
+
+    let showSlides = n => {
+        let i;
+        let slides = document.getElementsByClassName('slider__item');
+        let dots = document.getElementsByClassName('slider-dots__item');
+
+        if (n > slides.length) {
+            sliderIndex = 1;
+        } else {
+            sliderIndex = slides.length;
+        }
+
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = 'none';
+        }
+
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
 }
+
+module.exports = slider;
